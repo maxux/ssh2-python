ae8ce1dc94cdfded37ad475002c8977ef146ae91
Maxime Daniel <root@maxux.net>
2018-03-27 16:24:49 +0200

    Update travis script for local usage

--- ci/travis/build-manylinux.sh
+++ ci/travis/build-manylinux.sh
@@ -6,6 +6,6 @@ rm -rf build dist
 
 docker pull $docker_tag || echo
 docker build --cache-from $docker_tag ci/docker/manylinux -t $docker_tag
-docker push $docker_tag
+# docker push $docker_tag
 docker run --rm -v `pwd`:/io $docker_tag /io/ci/travis/build-wheels.sh
 ls wheelhouse/
